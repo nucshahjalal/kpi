2ce183bbc2dec0fe5942d223bc87fd98283f7d63
Motor-WAD-Shah Jalal <Shah.Jalal@aci-bd.com>
2026-04-20 19:07:46 +0600

    file update

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -15,29 +15,32 @@ class Employee extends Authenticatable
     use HasFactory;use Notifiable;
     protected $guarded = [];
 
-   public static function getEmployeeList()
-    {
-        return Employee::from('employees as E')
-            ->join('criterias AS C', 'E.id', '=', 'C.emp_id') 
-            ->select(
-                'E.id',
-                'E.full_name',
-                'E.staff_id',
-                'E.supervisor_name',
-                'E.supervisor_id',
-                'E.designation',
-                'C.label_name',
-                'C.criteria',
-                'C.target',
-                'C.score',
-                'C.actual',
-                'C.weight',
-                'C.type' 
-            )
-            ->orderBy('C.type')
-            ->orderBy('E.created_at', 'desc')
-            ->get()
-            ->groupBy('type'); // এখানে কালেকশন লেভেলে গ্রুপ করা হয়েছে
-    }
+ public static function getEmployeeList($emp_id = 58) 
+{
+    return Employee::from('employees as E')
+        ->join('criterias AS C', 'E.id', '=', 'C.emp_id') 
+        ->select(
+            'E.id',
+            'E.full_name',
+            'E.staff_id',
+            'E.supervisor_name',
+            'E.supervisor_id',
+            'E.designation',
+            'C.label_name',
+            'C.criteria',
+            'C.target',
+            'C.score',
+            'C.actual',
+            'C.weight',
+            'C.type' 
+        )
+        ->where('C.emp_id', $emp_id) 
+        ->orderBy('C.type')
+        ->orderBy('C.label_name') 
+        ->get()
+        ->groupBy(['type', 'label_name']); 
+}
+
+
 
 }
